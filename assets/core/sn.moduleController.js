3f7e0d3cb0f197188fcfccf30a229835a3ed8d1f
Holger Hellinger <hhellinger@sapient.com>
2015-01-05 10:37:12 +0100

    Add dependencies handling to controller
    - allows handling of load sequences now

--- assets/core/sn.moduleController.js
+++ assets/core/sn.moduleController.js
@@ -4,7 +4,6 @@
  *
  * @dependencies:
  * - jquery
- * - shims
  *
  * @project Generic
  * @date 2013
@@ -51,8 +50,10 @@ if (typeof window.sn === "undefined") {
         /**
          * List container for all registered modules
          */
-        var modules = [], plugins = [];
-
+        var modules = [], plugins = [], initializedPlugins = [],
+        // contains all modules waiting for their dependencies to be initialized for a single initAllModule() call.
+        // key is the name of the module teh module waits for, value the module registration module.
+            modulesWaitingForDependencies = {};
         /**
          * TBD - eventually move to a global sn.config object
          *
@@ -76,6 +77,41 @@ if (typeof window.sn === "undefined") {
          */
         var getRegisteredModules = function () {
             return modules;
+        };
+
+        /**
+         * getter method for all registered modules name
+         */
+        var getRegisteredModulesNames = function () {
+            var modulesName = [];
+            $.each(modules, function (index, element) {
+                var hasName = element.hasOwnProperty('name');
+                if (hasName) {
+                    modulesName.push(element.name);
+                }
+            });
+            return modulesName;
+        };
+
+        /**
+         * getter method for all registered plugins
+         */
+        var getRegisteredPlugins = function () {
+            return plugins;
+        };
+
+        /**
+         * getter method for all registered plugins name
+         */
+        var getRegisteredPluginsNames = function () {
+            var pluginsName = [];
+            $.each(plugins, function (index, element) {
+                var hasName = element.hasOwnProperty('name');
+                if (hasName) {
+                    pluginsName.push(element.name);
+                }
+            });
+            return pluginsName;
         };
 
         /**
@@ -115,7 +151,7 @@ if (typeof window.sn === "undefined") {
          */
         var processFunctionList = function (module, fArray, context) {
             var i, c = fArray.length;
-            for (i = 0; i < c; (i+=1)) {
+            for (i = 0; i < c; (i += 1)) {
                 module[fArray[i]](context);
             }
         };
@@ -134,20 +170,85 @@ if (typeof window.sn === "undefined") {
             }
         };
 
+        var pluginInitialized = function pluginInitialized(module) {
+            var i = 0;
+            initializedPlugins.push(module.name);
+            if (module.name in modulesWaitingForDependencies) {
+                for (i = 0; i < modulesWaitingForDependencies[module.name].length; i += 1) {
+                    if (!isModuleInitialized(modulesWaitingForDependencies[module.name][i].name)) {
+                        initializePluginWhenAllDependenciesAreInitialized(modulesWaitingForDependencies[module.name][i]);
+                    }
+                }
+                delete modulesWaitingForDependencies[module.name];
+            }
+        };
+
+        var clearInitializedPluginsList = function clearInitializedPluginsList() {
+            initializedPlugins = [];
+        };
+
+        var isModuleInitialized = function (moduleName) {
+            return initializedPlugins.indexOf(moduleName) !== -1;
+        };
+
+        var initializePluginWhenAllDependenciesAreInitialized = function initializePluginWhenAllDependenciesAreInitialized(module, context) {
+            var i = 0,
+                dependenciesLength = module.dependencies.length,
+                missingModules = 0;
+            for (; i < dependenciesLength; i += 1) {
+                if (!isModuleInitialized(module.dependencies[i])) {
+                    missingModules += 1;
+                    modulesWaitingForDependencies[module.dependencies[i]] = modulesWaitingForDependencies[module.dependencies[i]] || [];
+                    modulesWaitingForDependencies[module.dependencies[i]].push(module);
+                }
+            }
+            if (missingModules === 0) {
+                initializeModule(module, context);
+            }
+        };
+
+        var initializeModule = function initializeModule(module, context) {
+            if (module.type === "plugin") {
+                applyPlugin(module, context);
+            } else if (module.type === "initializer") {
+                processFunctionList(module.module, module.functionList, context);
+            }
+        };
+
+        function joinModuleNames(modules) {
+            var i = 0,
+                moduleNames = [],
+                length = modules.length;
+            for (; i < length; i += 1) {
+                if (moduleNames.indexOf(modules[i].name) === -1) {
+                    moduleNames.push(modules[i].name);
+                }
+            }
+            return moduleNames.join(", ");
+        }
 
         /**
          * Initialize all known modules.
          * @param [context] optional DOM context for the module group to be (re-)initialized
          */
         var initAllModules = function (context) {
+            modules = modules.sort();
+            //clean the initialized plugins list because only the current context is of interest.
+            clearInitializedPluginsList();
             var m, moduleCount = modules.length;
 
             /* loop through all modules and initialize it (call jQuery plugins) */
-            for (m = 0; m < moduleCount; (m+=1)) {
-                if (modules[m].type === "plugin") {
-                    applyPlugin(modules[m], context);
-                } else if (modules[m].type === "initializer") {
-                    processFunctionList(modules[m].module, modules[m].functionList, context);
+            for (m = 0; m < moduleCount; (m += 1)) {
+                if (modules[m].dependencies && modules[m].dependencies.length > 0) {
+                    initializePluginWhenAllDependenciesAreInitialized(modules[m], context);
+                } else {
+                    initializeModule(modules[m], context);
+                    pluginInitialized(modules[m]);
+                }
+            }
+            for (var key in modulesWaitingForDependencies) {
+                if (modulesWaitingForDependencies.hasOwnProperty(key)) {
+                    window.console.error("Some modules are still waiting for '" + key + "' to be initialized: " + joinModuleNames(modulesWaitingForDependencies[key]) + ".");
                 }
             }
         };
@@ -165,7 +266,7 @@ if (typeof window.sn === "undefined") {
             var p, pluginCount = plugins.length;
 
             /* loop through all modules and initialize it (call jQuery plugins) */
-            for (p = 0; p < pluginCount; (p+=1)) {
+            for (p = 0; p < pluginCount; (p += 1)) {
                 applyPlugin(plugins[p], context);
             }
         };
@@ -186,20 +287,22 @@ if (typeof window.sn === "undefined") {
          * @param module - module object that is passed for initialization
          * @param registerType - either a css selector (for plugins) or an array of functions (simple modules)
          * @param [opt] - optional options object only for plugin initialization calls
+         * @param [dependencies] - optional array containing the name of modules that must be initialized before
+         *                          the registered one is initialized.
          */
-        var register = function (name, module, registerType, opt) {
+        var register = function (name, module, registerType, opt, dependencies) {
             var simple = $.isArray(registerType),
                 options = opt || {},
                 selector, functionList, registerObject;
 
             /* make sure we have everything we need for registration */
-            if(typeof name === "undefined" || typeof name !== "string") {
+            if (typeof name === "undefined" || typeof name !== "string") {
                 throw "No module name defined";
             }
-            if(typeof module !== "object") {
+            if (typeof module !== "object") {
                 throw "No object defined for module registration";
             }
-            if(typeof registerType === "undefined") {
+            if (typeof registerType === "undefined") {
                 throw "No default css selector or array of functions defined";
             }
 
@@ -208,7 +311,7 @@ if (typeof window.sn === "undefined") {
                 /* when not a simple module, the registerType is defined as a css selector */
                 selector = registerType;
                 /* if we have some overruling configuration, replace defaults */
-                if(typeof moduleMapping[name] !== "undefined") {
+                if (typeof moduleMapping[name] !== "undefined") {
                     selector = moduleMapping[name].selector;
                     options = moduleMapping[name].options;
                 }
@@ -217,10 +320,11 @@ if (typeof window.sn === "undefined") {
                 createjQueryPlugin(module, name);
 
                 registerObject = {
-                    name     : name,
-                    selector : selector,
-                    options  : options,
-                    type     : "plugin"
+                    name: name,
+                    selector: selector,
+                    options: options,
+                    type: "plugin",
+                    dependencies: dependencies || []
                 };
 
                 /* modules contains the initialization stack ... */
@@ -232,10 +336,10 @@ if (typeof window.sn === "undefined") {
                 functionList = registerType;
 
                 registerObject = {
-                    name         : name,
-                    module       : module,
-                    functionList : functionList,
-                    type         : "initializer"
+                    name: name,
+                    module: module,
+                    functionList: functionList,
+                    type: "initializer"
                 };
 
                 modules.push(registerObject);
@@ -250,12 +354,18 @@ if (typeof window.sn === "undefined") {
          * @see www.wait-till-i.com/2007/08/22/again-with-the-module-pattern-reveal-something-to-the-world/
          */
         return {
-            register : register,
-            initAllModules : initAllModules,
-            initPlugins : initPlugins,
-            getRegisteredModules : getRegisteredModules
+            register: register,
+            initAllModules: initAllModules,
+            initPlugins: initPlugins,
+            getRegisteredModules: getRegisteredModules,
+            getRegisteredModulesNames: getRegisteredModulesNames,
+            getRegisteredPlugins: getRegisteredPlugins,
+            getRegisteredPluginsNames: getRegisteredPluginsNames
         };
     }());
 }(window, document, jQuery, sn));
 /* initialize all modules on document.ready */
-jQuery(function(){'use strict';sn.moduleController.initAllModules();});
+jQuery(function () {
+    'use strict';
+    sn.moduleController.initAllModules();
+});
